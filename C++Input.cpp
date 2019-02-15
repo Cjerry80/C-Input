d7d0c1dc1c1806cf11e852992f4255d9f79cc1f2
cjerry80 <cjerry80@cs3002c.acad.fullerton.edu>
2019-02-14 17:08:44 -0800

    Modified C++Input

--- C++Input.cpp
+++ C++Input.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <fstream>
-
+#include <cstring>
+#include <string>
 using namespace std;
 
 int main(){
 
-
-
+string line;
+int number_of_records = 0;
+ifstream inData;
+inData.open("data.txt");
+cout << "Hi" << endl;
+   while (getline(inData, line))
+       ++number_of_records;
+   cout << "Number of lines in text file: " << number_of_records << endl;
+   inData.close();
 return 0;
 }
+
+struct  PERSON {
+    char  Name[20];
+    float Balance;
+  };
