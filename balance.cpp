28239d2d4255c26e5c965e7ab63ee51f9faefd52
cjerry80 <cjerry80@cs3006b.acad.fullerton.edu>
2019-04-16 17:02:32 -0700

    Modified balance

--- balance.cpp
+++ balance.cpp
@@ -9,131 +9,117 @@ struct  PERSON {
     float Balance;
 };
 
-void Readfile(int r, PERSON array[]);
-void Display(int r, PERSON array[]);
-void findRichest(int r, PERSON array[]);
-void Deposit(string custname, int r, PERSON array[]);
-void Newcopy(string file, int r, PERSON array[]);
-PERSON readData(int N);
-void Deposit(arr, N, custName, amount);
+int getnumberofRecords();
+void Readfile(int N, PERSON P[]);
+void Display(int N, PERSON P[]);
+void findRichest(int N, PERSON P[]);
+void Newcopy(string file, int N, PERSON P[]);
+PERSON * readData(int N);
+void Deposit(Person P[], int N, string custName,float amount);
+
 int main(){
 
-string line;
-int records;
-string custname;
+PERSON * P = nullptr;
+P = readData(N);
+string custName;
+float amount;
 
-fstream file ("data.txt");
-if (file.is_open()){
-  while (getline(file, line))
-  {
-  ++records;
-  }
-  cout << "The number of records is: " << records << endl;
-  file.close();
-}
+Readfile(N, P);
+Display(N, P);
+findRichest(N, P);
 
-
-PERSON array[records];
-Readfile(records, array);
-Display(records, array);
-findRichest(records, array);
-
-cout << "Enter your full name: ";
-getline(cin, custname);
-Deposit(custname, records, array);
-Newcopy("data.txt", records, array);
+cout << "Enter name: " << endl;
+cin.ignore();
+getline(cin, custName);
+cout << "Amount: " << endl;
+cin >> amount;
+Deposit(P, N, custName, amount);
+Newcopy("data.txt", N, P);
 
 return 0;
 
 }
 
 PERSON * readData(int N){
-
+N = getnumberofRecords();
 ifstream infile;
 string firstn;
 string lastn;
+string line;
+Person * P = null;
 infile.open("data.txt");
-int * p;
+A = new PERSON[N];
+
 
 for (int i = 0; i < N; i++){
-infile >> firstn;
-infile >> lastn;
-infile >> p[i].Balance;
-string name = firstn + " " + lastn;
-strcpy (p[i].Name, name.c_str());
-}
+  infile >> firstn;
+  infile >> lastn;
+  infile >> A[i].balance;
+  getline(infile, line);
+  strncpy(A[i].Name, firstn.c_str(), 9);
+  strcat(A[i].Name, " ");
+  strncat(A[i].Name, lastn_c_str(),10);
+  }
   infile.close();
-  return p;
+
+return A;
 }
 
-void Readfile(int r, PERSON array[]){
-ifstream infile;
-string firstn;
-string lastn;
-float bal;
 
+int getnumberofRecords(){
 
-infile.open("data.txt");
+  int records;
 
-for (int i = 0; i < r; i++){
-infile >> firstn;
-infile >> lastn;
-infile >> bal;
-string name = firstn + " " + lastn;
-strcpy (array[i].Name, name.c_str());
-array[i].Balance = bal;
-}
-infile.close();
+  fstream file ("data.txt");
+  if (file.is_open()){
+    while (getline(file, line))
+    {
+    ++records;
+    }
+    file.close();
 
+  return records;
 }
 
-void Display(int r, PERSON array[]){
-cout << "Name" << "               " << "Balance" << endl;
-cout << "----------------------------------" << endl;
-for(int i = 0; i < r; i++){
-cout << array[i].Name << "     " << array[i].Balance << endl;
+void Display(int N, PERSON P[]){
+cout << "Display: " << endl;
+for(int i = 0; i < N; i++){
+cout << P[i].Name << " " << P[i].balance << endl;
 }
 
 }
-
-void findRichest(int r, PERSON array[]){
-int temp = 0;
-int counter = 0;
-
-for (int i = 0; i < r; i++){
-  if (array[i].Balance > temp ){
-    temp = array[i].Balance;
-    counter = i;
+void findRichest(int N, PERSON P[]){
+  int temp = 0;
+  int counter = 0;
+  cout << "Find Richest: " << endl;
+  for (int i = 0; i < r; i++){
+    if (P[i].balance > temp ){
+      temp = P[i].balance;
+      counter = i;
+    }
   }
+  cout << "Highest Balance: " << P[counter].Name << endl;
 }
-cout << "The customer with maximum balance is " << array[counter].Name << endl;
-
-}
-void Deposit(arr, N, custName, amount){
-
-}
-void Deposit(string custname, int r, PERSON array[]){
-
-int depositmoney;
-
-for (int i = 0; i < r; i++){
-  if (custname != array[i].Name){
-    cout << "Record not found." << endl;
+void Deposit(Person P[], int N, string custName, float amount){
+for (int i = 0; i < N; i++ ){
+  if (custName == P[i].Name){
+    P[i].balance = P[i].balance + amount
+    cout << "New Balance: " << P[i].balance << endl;
   }
-  else{
-    cout << custname << ", How much would you like to deposit? ";
-    cin >> depositmoney;
-    array[i].Balance = array[i].Balance + depositmoney;
-    cout << "Now your new balance is " << array[i].Balance << endl;
+  else {
+    cout << "Record not found. " << endl;
   }
 }
 }
-void Newcopy(string file, int r, PERSON array[]){
+
+
+void Newcopy(string file, int N, PERSON P[]){
 
 ofstream outfile;
+cout << "Newcopy: " << endl;
 outfile.open("data.txt");
 for (int i = 0; i < r; i++){
-  outfile << array[i].Name << " " << array[i].Balance << endl;
+  outfile << P[i].Name << " " << P[i].balance << endl;
 }
 outfile.close();
 cout << "File Updated.." << endl;
